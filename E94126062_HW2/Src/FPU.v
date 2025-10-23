f1d9fad4084da89f79c778b84e35bd3c093f2981
x1angooooo <jeffrey1219a@gmail.com>
2025-10-23 13:35:17 +0800

    Add files via upload

---
+++ E94126062_HW2/Src/FPU.v
@@ -0,0 +1,110 @@
+`include "../Src/FPU_comparator.v"
+`include "../Src/FPU_adder.v"
+module FPU(
+    input clk,
+    input rst,
+    input enable,
+    input [1:0] instruction,
+    input [31:0] ai,
+    input [31:0] bi,
+    output reg [31:0] co,
+    output reg valid
+);
+
+//put ur design here
+
+wire sub;
+wire mode;
+wire [31:0] adder_output;
+wire adder_output_ready;
+wire [31:0] comparator_output;
+
+FPU_adder adder(
+	.input_a(ai),
+	.input_b(bi),
+	.enable(enable),
+	.sub(sub),
+	.clk(clk),
+	.rst(rst),
+	.output_c(adder_output),
+	.output_c_ready(adder_output_ready)
+);
+
+FPU_comparator comparator(
+    .A(ai),
+    .B(bi),
+    .mode(mode), 
+    .answer(comparator_output)       
+);
+
+assign sub = (instruction == 2'd3);
+assign mode = (instruction == 2'd1);
+
+
+parameter waitanswer = 2'b00;
+parameter adderoutput = 2'b01;
+parameter comparatoroutput = 2'b10;
+
+reg [1:0] current_state, next_state;
+
+always @(posedge clk or posedge rst) begin
+	if (rst) begin
+		co <= 32'b0;
+		valid <= 0;
+		current_state <= waitanswer;
+	end
+	else begin
+		current_state <= next_state;
+		case (current_state) 
+			waitanswer: begin
+				valid <= 0;
+				co <= 32'b0;
+			end
+			adderoutput: begin
+				if (adder_output_ready) begin
+					valid <= 1;
+					co <= adder_output;
+				end
+				else begin
+					valid <= 0;
+					co <= 0;
+				end
+			end
+			comparatoroutput: begin
+				valid <= 1;
+				co <= comparator_output;
+			end
+			default: begin
+				valid <= 0;
+				co <= 32'b0;
+			end
+		endcase
+	end
+end
+
+always @(*) begin
+
+	case(current_state)
+		waitanswer: begin
+			if (enable) begin
+				next_state = (instruction == 2'b00 || instruction == 2'b01) ? comparatoroutput : adderoutput;
+			end
+			else begin
+				next_state = current_state;
+			end
+		end
+		adderoutput: begin
+			next_state = (adder_output_ready) ? waitanswer : adderoutput;
+		end
+		comparatoroutput: begin
+			next_state = waitanswer;
+		end
+		default: begin
+			next_state = waitanswer;
+		end
+	endcase
+end
+
+
+
+endmodule
---
+++ E94126062_HW2/Src/FPU_comparator.v
@@ -0,0 +1,76 @@
+module FPU_comparator(
+    input [31:0] A,
+    input [31:0] B,
+    input mode, 
+    output reg [31:0] answer   
+);
+
+    reg a_sign, b_sign;
+    reg [7:0] a_exp, b_exp;
+    reg [22:0] a_fracrion, b_fraction;
+
+    reg a_is_NaN,b_is_NaN;
+
+    parameter fmax = 1'b1;
+    parameter fmin = 1'b0;
+
+//put ur design here 
+    always @(*) begin
+        // Unpack
+        a_sign = A[31];
+        b_sign = B[31];
+
+        a_exp = A[30:23];
+        b_exp = B[30:23];
+
+        a_fracrion = A[22:0];
+        b_fraction = B[22:0];
+
+        a_is_NaN = (a_exp == 8'hFF) && (a_fracrion != 0);
+        b_is_NaN = (b_exp == 8'hFF) && (b_fraction != 0);
+        // Compare 
+        // special case NaN
+
+        if (a_is_NaN || b_is_NaN) begin
+            answer = 32'hFFC00000;
+        end
+
+        else if (a_sign ^ b_sign) begin
+            case (mode) 
+                fmax:
+                    answer = (a_sign) ? B : A;
+                fmin:
+                    answer = (a_sign) ? A : B;
+                default:
+                    answer = 32'd0;
+            endcase
+        end
+
+        else if ( a_exp != b_exp ) begin
+            case ({mode, a_sign}) 
+                {fmax, 1'b0}, {fmin, 1'b1}:
+                    answer = (a_exp > b_exp) ? A : B;
+                {fmin, 1'b0}, {fmax, 1'b1}:
+                    answer = (a_exp > b_exp) ? B : A;
+                default:
+                    answer = 32'd0;
+            endcase
+        end
+
+        else if ( a_fracrion != b_fraction ) begin
+            case ({mode, a_sign})
+                {fmax, 1'b0}, {fmin, 1'b1}:
+                    answer = (a_fracrion > b_fraction) ? A : B;
+                {fmin, 1'b0}, {fmax, 1'b1}:
+                    answer = (a_fracrion > b_fraction) ? B : A;
+                default:
+                    answer = 32'd0;
+            endcase
+        end
+
+        else begin
+            answer = A;
+        end
+
+    end
+endmodule
